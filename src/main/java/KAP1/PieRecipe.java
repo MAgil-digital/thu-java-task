9c93f46a9b87fa995d9a162e46e141a82f93ee2c
mertagil <MAgil.network@gmail.com>
2025-08-08 15:29:30 +0200

    Created PieRecipe.java. class

---
+++ src/main/java/KAP1/PieRecipe.java
@@ -0,0 +1,44 @@
+package src.main.java.KAP1;
+
+public class PieRecipe {
+    public static void main(String[] args) {
+
+        System.out.println("Apple Pie Receipt:");
+        System.out.println("==================");
+
+        // declarate variable "eggs" of type "int"
+        int eggs;
+
+        // assign value 4 to "eggs"
+        eggs = 4;
+
+        System.out.println("- " + eggs + " Eggs");
+
+        // declarate variable "milk" of type "double"
+        double milk;
+
+        // and assign value 100 in one line
+        milk = 100;
+
+        System.out.println("- " + milk + " ml Milk" );
+
+        // declarate variable "flour" of type "double"
+        double flour;
+
+        // assign value 200 to "flour"
+        flour = 200;
+
+        // add 150 to "flour"
+        flour += 150;
+
+        System.out.println("- " + flour + " mg Flour");
+
+        // delcarate variable "apple" of type "int"
+        int apple;
+
+        // assign the value of "eggs" to "apple"
+        apple = eggs;
+
+        System.out.println("- " + apple + " Apples");
+    }
+}
