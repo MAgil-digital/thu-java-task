70087587d552fde0e1cbda7f0e6f55001a68270b
mertagil <MAgil.network@gmail.com>
2025-08-08 15:15:22 +0200

    Created DeclarationAndInitialization.java class

---
+++ src/main/java/KAP1/DeclarationAndInitialization.java
@@ -0,0 +1,24 @@
+package src.main.java.KAP1;
+
+public class DeclarationAndInitialization {
+    public static void main (String [] args){
+
+        //mark where to declare and initialize
+
+        int number; //declaration
+        String word; //declaration
+        double realNumber;//declaration
+        number = 1; //initialization
+        word = "Hello"; //initialization
+        realNumber = 0.2; //initialization
+        System.out.println(number);
+        System.out.println(word);
+        System.out.println(realNumber);
+        int anotherNumber = 5; //declaration & initialization
+        String anotherWord = "You"; //declaration 6 initialization
+        double anotherRealNumber = 8.15; // declaration & initialization
+        System.out.println(anotherNumber);
+        System.out.println(anotherWord);
+        System.out.println(anotherRealNumber);
+    }
+}
