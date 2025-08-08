2788bcb9c085934608b963b142969f2771f5057e
mertagil <MAgil.network@gmail.com>
2025-08-08 15:08:20 +0200

    Created JavaAndHisDatatypes.java class

---
+++ src/main/java/KAP1/JavaAndHisDatatypes.java
@@ -0,0 +1,24 @@
+package src.main.java.KAP1;
+
+public class JavaAndHisDatatypes {
+    public static void main(String[] args){
+
+        //whole numbers
+        int ganzeZahlen = 100;
+
+        //long floated numbers
+        double langeGleitkommazahlen = 1.202020202020202020;
+
+        // 7 Digits float Number
+        float siebenStellenGleitkommezahl = 1.12345678f;
+
+        // true or false
+        boolean wahrFalsch = true;
+
+        // Strings
+        String texte = "Das ist ein Text";
+
+        //Character
+        Character einZeichen = '§';
+}
+}
