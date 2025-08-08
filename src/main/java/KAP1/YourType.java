216ac9a15700dd6c25145687000a6c127d1846a5
mertagil <MAgil.network@gmail.com>
2025-08-08 15:11:23 +0200

    Created YourType.java class

---
+++ src/main/java/KAP1/YourType.java
@@ -0,0 +1,34 @@
+package src.main.java.KAP1;
+
+public class YourType {
+    public static void main(String[] args){
+
+        // here are some Datatypes for the following usecases
+
+        // age of a person
+        int age = 20;
+
+        //The annual salary in whole euro
+        int salary = 30000;
+
+        //a person's gender
+        String gender = "Male";
+
+        //A person's title (Mr., Mrs., etc.)
+        String title = "Mrs";
+
+        //The Eulerian number e with 14 decimal places
+        double eulerianNumber = 2.71828182845905;
+
+        //The weight in kg
+        float weight = 30.2f;
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(gender);
+        System.out.println(title);
+        System.out.println(eulerianNumber);
+        System.out.println(weight);
+
+    }
+}
